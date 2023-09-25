21f05e50231e699351424fe874a4da39c12b074b
SchultestUser <bruce.willer@stud.bs7-augsburg.de>
2023-09-25 12:52:00 +0200

    wtf

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int schaltJahr = 1600;
+        String outputMessage = "";
+
+        if (schaltJahr % 400 != 0 && schaltJahr % 4 == 0 && schaltJahr % 100 != 0) {
+            outputMessage = "Schaltjahr";
+        } else {
+            outputMessage = "kein Schaltjahr";
+        }
+
+
+        System.out.println(outputMessage);
     }
 }
